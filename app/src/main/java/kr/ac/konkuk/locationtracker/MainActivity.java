cc512f746e5261cbbd2eba8c7fd7056a05afa17b
Chanhee <chan420420@gmail.com>
2020-10-24 06:55:58 +0900

    install Fused Location Client

--- app/src/main/java/kr/ac/konkuk/locationtracker/MainActivity.java
+++ app/src/main/java/kr/ac/konkuk/locationtracker/MainActivity.java
@@ -6,18 +6,36 @@ package kr.ac.konkuk.locationtracker;
         import android.widget.Switch;
         import android.widget.TextView;
 
+        import com.google.android.gms.location.FusedLocationProviderClient;
+        import com.google.android.gms.location.LocationRequest;
+
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates, tv_address;
+    //HOW OFTEN
+    public static final int DEFAULT_UPDATE_INTERVAL = 30;
+    public static final int FAST_UPDATE_INTERVAL = 5;
 
+    //ui
+    TextView tv_lat, tv_lon, tv_altitude, tv_accuracy, tv_speed, tv_sensor, tv_updates, tv_address;
     Switch sw_locationsupdates, sw_gps;
+
+    // variable for remember if we are tracking location or not
+    boolean updateOn = false;
+
+    //Location Request is a config file for all setting realted ro FusedLocationProviderClient
+    LocationRequest locationRequest;
+
+    // Google's API for location services. the majority of the app functions using this class
+    FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //ui
+        /*
+         * ui
+         */
         tv_lat = findViewById(R.id.tv_lat);
         tv_lon = findViewById(R.id.tv_lon);
         tv_altitude = findViewById(R.id.tv_altitude);
@@ -28,6 +46,25 @@ public class MainActivity extends AppCompatActivity {
         tv_address = findViewById(R.id.tv_address);
         sw_locationsupdates = findViewById(R.id.sw_locationsupdates);
         sw_gps = findViewById(R.id.sw_gps);
+
+
+
+        /*
+        * set all propertiees of LocationRequest
+        */
+
+        locationRequest = new LocationRequest();
+
+        // 디폴트 위치기록 주기 30secs
+        locationRequest.setInterval(1000 * DEFAULT_UPDATE_INTERVAL);
+
+        // 더 빠른 주기록 기록시 5secs
+        locationRequest.setInterval(1000 * FAST_UPDATE_INTERVAL);
+
+        //high accuracy lowpower
+        locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+
 
 
 
